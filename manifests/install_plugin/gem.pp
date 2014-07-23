8c32c01e758ce98d31e9a1c0aa63cba84818ecf6
Gianni Carafa <gianni.carafa@srf.ch>
2014-07-24 01:51:48 +0200

    Revert "Feature gemversion"

--- manifests/install_plugin/gem.pp
+++ manifests/install_plugin/gem.pp
@@ -8,14 +8,42 @@
 #  ensure:      "present"(default) or "absent", install or uninstall a plugin
 #
 define fluentd::install_plugin::gem (
-    $ensure      = 'latest',
+    $ensure      = 'present',
     $plugin_name = $name,
 ) {
 
-    package { $plugin_name:
-      ensure   => $ensure,
-      provider => 'fluentgem',
-      notify   => Service["${fluentd::service_name}"];
+    case $::osfamily {
+        'debian': {
+            $fluent_gem_path = '/usr/lib/fluent/ruby/bin/fluent-gem'
+        }
+        'redhat': {
+            $fluent_gem_path = '/usr/lib64/fluent/ruby/bin/fluent-gem'
+        }
+        default: {
+            fail("${::osfamily} is currently not supported by this module")
+        }
     }
-
+    case $ensure {
+        present: {
+            exec {
+                "install_fluent-${plugin_name}":
+                    command => "${fluent_gem_path} install ${plugin_name}",
+                    user    => 'root',
+                    unless  => "${fluent_gem_path} list --local ${plugin_name} | /bin/grep -q ${plugin_name}",
+                    notify  => Service["${fluentd::service_name}"];
+            }
+        }
+        absent: {
+            exec {
+                "install_fluent-${plugin_name}":
+                    command => "${fluent_gem_path} uninstall ${plugin_name}",
+                    user    => 'root',
+                    unless  => "${fluent_gem_path} list --local ${plugin_name} | /bin/grep -qv ${plugin_name}",
+                    notify  => Service["${fluentd::service_name}"];
+            }
+        }
+        default: {
+            fail("ensure => ${ensure} is currently not supported by this module")
+        }
+    }
 }
