18416cb5e2feea59db93397ead2c9fc740358100
Cédric Barboiron <cedric.barboiron@oxalide.com>
2017-04-27 16:37:40 +0200

    install v2 by default
    delete old gpg key

--- manifests/install_repo/apt.pp
+++ manifests/install_repo/apt.pp
@@ -4,31 +4,17 @@
 #
 #
 class fluentd::install_repo::apt () {
-  case $::lsbdistcodename {
-    'wheezy': {
-      apt::source { 'treasure-data':
-        location    => 'http://packages.treasuredata.com/debian',
-        release     => 'lucid',
-        repos       => 'contrib',
-      }
-    }
-    default: {
-      apt::source { 'treasure-data':
-        location    => "http://packages.treasuredata.com/2/debian/${::lsbdistcodename}/",
-        repos       => 'contrib',
-      }
-    }
+  apt::source { 'treasure-data':
+    location => "http://packages.treasuredata.com/2/debian/${::lsbdistcodename}/",
+    repos    => 'contrib',
   }
 
   file { '/tmp/packages.treasure-data.com.key':
     ensure => file,
-    source => [
-      "puppet:///modules/fluentd/td-agent.key.${::lsbdistcodename}",
-      'puppet:///modules/fluentd/td-agent.key',
-    ],
-  }->
-  exec { "import gpg key Treasure Data":
-    command => "/bin/cat /tmp/packages.treasure-data.com.key | apt-key add -",
+    source => 'puppet:///modules/fluentd/td-agent.key',
+  }
+  -> exec { 'import gpg key Treasure Data':
+    command => '/bin/cat /tmp/packages.treasure-data.com.key | apt-key add -',
     unless  => "/usr/bin/apt-key list | grep -q 'Treasure Data'",
     notify  => Class['::apt::update'],
   }
